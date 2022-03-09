8599242e5f06dac34c899de4a551e33934a7848c
Panov Georgii <panovgeorgij819@gmail.com>
2022-03-09 09:14:54 +0500

    Добавление кода

--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
+# Code Michail Vorobiev
+# from django.db import models
+
+# Create your models here.
+# Code Panov Georgii
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
@@ -23,4 +28,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-# Create your models here.
