80300231b33911f9c75629a9ee0637567119df39
Panov Georgii <panovgeorgij819@gmail.com>
2022-05-30 19:24:59 +0500

    update setting_page

--- static/settings/settings.js
+++ static/settings/settings.js
@@ -2,7 +2,26 @@ let content_1 = document.querySelector('.content_settings_1');
 let content_2 = document.querySelector('.content_settings_2');
 let top_settings = document.querySelector('.top_settings');
 let account_settings = document.querySelector('.account_settings');
+let by_time = document.querySelector('.by_time');
 content_2.style.display = 'none';
+
+function showOption(el) {
+    if (el.selectedIndex === 4) {
+        by_time.style.display = 'block';
+    }
+
+    else if (el.selectedIndex === 1) {
+        by_time.style.display = 'none';
+    }
+
+    else if (el.selectedIndex === 2) {
+        by_time.style.display = 'none';
+    }
+
+    else if (el.selectedIndex === 3) {
+        by_time.style.display = 'none';
+    }
+}
 
 account_settings.onclick = function () {
     content_1.style.display = 'none';
